e08b0761047e3454d3cdce09b43d6bb7fda7e0cf
Андрей <31936190+JamboToYou@users.noreply.github.com>
2017-12-13 02:02:40 +0400

    1

--- RGR/RGR/Program.cs
+++ RGR/RGR/Program.cs
@@ -13,35 +13,22 @@ namespace RGR
     {
         public static void Main()
         {
-            XmlSchemaInference infer = new XmlSchemaInference();
-            XmlSchemaSet schemaSet =
-              infer.InferSchema(new XmlTextReader("Puzzles.xml"));
+            XmlReaderSettings setting = new XmlReaderSettings();
 
-            XmlWriter w = XmlWriter.Create("Puzzles.xsd");
-            foreach (XmlSchema schema in schemaSet.Schemas())
-            {
-                schema.Write(w);
-            }
-            w.Close();
-            XDocument xdoc = XDocument.Load("Puzzles.xsd");
-            Console.WriteLine(xdoc);
+            setting.DtdProcessing = DtdProcessing.Parse;
+            setting.ValidationType = ValidationType.DTD;
+            setting.ValidationEventHandler += new ValidationEventHandler(ValidationCallBack);
+
+            XmlReader reader = XmlReader.Create("D:\\edu\\prog\\ИТ\\InfTech\\RGR\\RGR\\Guitars.xml", setting);
+
+            while (reader.Read()) ;
+
             Console.ReadKey();
+        }
 
-            XDocument xDocument = XDocument.Load("Puzzles.xml");
-
-            XmlSchemaSet Schema = new XmlSchemaSet();
-            schemaSet.Add(null, "Puzzles.xsd");
-            try
-            {
-                xDocument.Validate(schemaSet, null);
-                Console.WriteLine("Проверка достоверности документа завершена успешно");
-            }
-            catch (XmlSchemaValidationException ex)
-            {
-                Console.WriteLine("Произошло исключение:   {0}", ex.Message);
-                Console.WriteLine("Документ не прошел проверку достоверности.");
-            }
-            Console.ReadKey();
+        private static void ValidationCallBack(object sender, ValidationEventArgs e)
+        {
+            Console.WriteLine("Validation Error: {0}", e.Message);
         }
     }
 }
